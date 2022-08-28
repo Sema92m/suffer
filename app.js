2817c4bfe5afb3121b998a522e998231597aef14
Simon <sema92m@wp.pl>
2022-08-29 00:30:02 +0200

    Add aunter seconds, add animations(bgMove, blockRotate,characterRotate)

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 const btnRef = document.getElementById("btn_ref");
 const block = document.getElementById("block");
-const AUDIO = new Audio('./audio/backgroundmusic.mp3');
-let counter = 0;
-let counterDisplayElem = document.querySelector("counter-display");
+const AUDIO = new Audio("./audio/backgroundmusic.mp3");
+let counterDisplayElem = document.getElementById("counter-display");
+let gameoverAlert = document.getElementById("gameover-alert");
+let count = 0;
 
 function moveLeft() {
     let left = parseInt(
@@ -13,7 +14,6 @@ function moveLeft() {
         character.style.left = left + "px";
     }
 }
-
 function moveRight() {
     let left = parseInt(
         window.getComputedStyle(character).getPropertyValue("left")
@@ -33,46 +33,46 @@ document.addEventListener("keydown", (event) => {
     }
 });
 
+function counterSeconds() {
+    counterDisplayElem.innerHTML = ++count;
+}
+setInterval(counterSeconds,1000);
+
+
+
 block.addEventListener("animationiteration", () => {
     let random = Math.floor(Math.random() * 3);
     left = random * 100;
     block.style.left = left + "px";
     // audioScore.play();
-    counter++;
 });
 
-// function updateDisplay() {
-//     counterDisplayElem.innerHTML = counter;
-// }
-
-// setInterval(function () {
-//     let characterLeft = parseInt(
-//         window.getComputedStyle(character).getPropertyValue("left")
-//     );
-//     let blockLeft = parseInt(
-//         window.getComputedStyle(block).getPropertyValue("left")
-//     );
-//     let blockTop = parseInt(
-//         window.getComputedStyle(block).getPropertyValue("top")
-//     );
-//     if (characterLeft == blockLeft && blockTop < 500 && blockTop > 300) {
-//         btnRef.style.display = "block";
-//         block.style.animation = "none";
-//         alert("Game over. Score: " + counter);
-//     }
-// }, 10);
-
+setInterval(function () {
+    let characterLeft = parseInt(
+        window.getComputedStyle(character).getPropertyValue("left")
+    );
+    let blockLeft = parseInt(
+        window.getComputedStyle(block).getPropertyValue("left")
+    );
+    let blockTop = parseInt(
+        window.getComputedStyle(block).getPropertyValue("top")
+    );
+    if (characterLeft == blockLeft && blockTop < 500 && blockTop > 300) {
+        btnRef.style.display = "block";
+        block.style.animation = "none";
+        gameoverAlert.style.display = "block";
+        counterDisplayElem.style.left = "20px";
+        counterDisplayElem.style.scale = "2";
+    }
+}, 10);
 
 document.getElementById("left").addEventListener("touchstart", moveLeft);
 document.getElementById("right").addEventListener("touchstart", moveRight);
 
-
 function playSound() {
-    if (document.getElementById('audio-on').checked){
+    if (document.getElementById("audio-on").checked) {
         AUDIO.play();
-        AUDIO.loop();
-    }else{
+    } else {
         AUDIO.pause();
     }
 }
-
